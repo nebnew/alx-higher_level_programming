d838686d7e454ead66f26bc1188d2fe4b5380a8b
nebiyat <nebiyats@gmail.com>
2022-12-01 11:22:47 -0800

    task 4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-    n = len(sys.argv)
+
+if __name__ == '__main__':
+    av = sys.argv
+    l_av = len(av)
     sum = 0
-    for i in range(1, n):
-        sum += int(sys.argv[i])
+
+    if l_av > 1:
+        for i in range(1, l_av):
+            sum += int(av[i])
+
     print(sum)
